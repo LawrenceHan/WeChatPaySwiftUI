ebce537d54f8d74c605ff50d32640c170b28e1fd
Hanguang <yangfei6565@163.com>
2019-10-11 11:42:19 +0800

    test 1

---
+++ WeChatPaySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeChatPaySwiftUI
+//
+//  Created by Hanguang on 2019/10/11.
+//  Copyright © 2019 hanguang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
